e850ffd9b4e7c35fd5a412812ee352e9ef22368f
saimundra <saimundra77792@gmail.com>
2025-06-19 14:34:33 +0545

    DAY 10

---
+++ Day 10/filehandling.py
@@ -0,0 +1,12 @@
+#filehandling
+#you can create files,readfiles,updatefiles and delete files using different functions
+#open() parameters-filename and mode(types of moses 4)
+#read-"r"
+#write-"w"
+#append-"a"
+#create-"x"
+#text -"t"
+#binary -"b"
+
+f = open("demofile.txt","w")
+f.write('test') 
